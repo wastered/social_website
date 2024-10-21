ed4e9aebe392675f0d22ed619f20d2eeddeda241
wastered <nonamelite@mail.ru>
2024-10-21 17:31:02 +0300

    Добавлена обработка нажатие лайка с помощью fetchAPI

--- server/apps/images/views.py
+++ server/apps/images/views.py
@@ -1,6 +1,8 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 
 from .forms import ImageCreateForm
 from .models import Image
@@ -42,3 +44,24 @@ def image_detail(request, id, slug):
                       'section': 'images',
                       'image': image,
                   })
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')  # "like" or "unlike"
+
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+
+    return JsonResponse({'status': 'error'})
