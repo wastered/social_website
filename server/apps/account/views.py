89a31d765b1d764af4f455d54a1d0b8bec2bd69f
wastered <nonamelite@mail.ru>
2024-10-26 17:13:50 +0300

    Добавлена система подписки пользователей друг на друга

--- server/apps/account/views.py
+++ server/apps/account/views.py
@@ -2,11 +2,12 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
-from .models import Profile
+from .models import Profile, Contact
 
 
 @login_required()
@@ -117,3 +118,30 @@ def user_detail(request, username):
                       'section': 'people',
                       'user': user
                   })
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+
+            if user_id == request.user.id:
+                return JsonResponse({'status': 'error'})
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user,
+                )
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+
+    return JsonResponse({'status': 'error'})
