bd7929fc5c9a9ccc78254eef179f38eb192d6116
wastered <nonamelite@mail.ru>
2024-10-27 15:27:16 +0300

    Добавлено отображение потока активности

--- server/apps/account/views.py
+++ server/apps/account/views.py
@@ -8,17 +8,28 @@ from django.views.decorators.http import require_POST
 
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile, Contact
+from ..actions.models import Action
 from ..actions.utils import create_action
 
 
 @login_required()
 def dashboard(request):
+    # По умолчанию показать все действия
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+
+    if following_ids:
+        # Если пользователь подписан на других,
+        # то извлечь только их действия
+        actions = actions.filter(user_id__in=following_ids)
+
+    actions = actions.select_related('user', 'user__profile')[:10].prefetch_related('target')[:10]
     return render(request,
                   'account/dashboard.html',
                   context={
-                      'section': 'dashboard'
-                  }
-                  )
+                      'section': 'dashboard',
+                      'actions': actions,
+                  })
 
 
 def user_login(request):
